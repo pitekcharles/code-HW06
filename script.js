21b7ee83705485f29f5e8e70e205a1a7ca5c3f0a
unknown <charles.pitek@gmail.com>
2019-12-30 07:49:20 -0800

    Started adding potential functions to js file with comments about what each one will become

---
+++ script.js
@@ -0,0 +1,25 @@
+//Variable Creation
+
+//Function Creation
+
+//Function for when the search is used
+function searchEnter () {
+
+}
+
+//Function to populate current data to jumbotron
+function jumboPop() {
+
+}
+
+//Function to populate side bar of saved cities
+function sidePop() {
+
+}
+
+//Function for when city on sidebar is clicked
+function sideClick () {
+
+}
+
+//Function Call outs
