19a521aa7f7e71341754ef23fa21297e9ffa8ff5
unknown <charles.pitek@gmail.com>
2019-12-30 11:26:42 -0800

    Fixed issue where site would not load while html file waas linked

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-//Variable Creation
-var tempCurrent = 0;
+console.log("crap")
+var temp = 0;
 var date = "todays date";
-var location = "everywhere";
-var humidityCurrent = 0;
-var windCurrent = 0;
-var UVCurrent = 0;
+var locationCurrent = "everywhere";
+var humidity = 0;
+var wind = 0;
+var UV = 0;
 //Function Creation
 
 //Function for when the search is used
-function searchEnter () {
+function searchEnter() {
 
 }
 
 //Function to populate current data to jumbotron
 function jumboPop() {
-    $("#tempCurrent").text("Temperature: " + tempCurrent);
-    $("#locationCurrent").text(Location + "(" + date + ")");
-    $("#humidCurrent").text("Humidity: " + humidityCurrent);
-    $("#windCurrent").text("Wind Speed: " + windCurrent);
-    $("#UVCurrent").text("UV Index: " + UVCurrent);
+    $("#tempCurrent").text("Temperature: " + temp);
+    $("#locationCurrent").text(locationCurrent + "(" + date + ")");
+    $("#humidCurrent").text("Humidity: " + humidity);
+    $("#windCurrent").text("Wind Speed: " + wind);
+    $("#UVCurrent").text("UV Index: " + UV);
 }
 
 //Function to populate side bar of saved cities
@@ -26,8 +26,8 @@ function sidePop() {
 
 }
 
-//Function for when city on sidebar is clicked
-function sideClick () {
+// //Function for when city on sidebar is clicked
+function sideClick() {
 
 }
 
