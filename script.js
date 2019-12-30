9ba5ec51d69916c5669c22711e38edede1f618e7
unknown <charles.pitek@gmail.com>
2019-12-30 12:46:19 -0800

    started work on the Ajax call to pull info for api and started work on printing those values to the screen

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-var temp = 0;
+var temperature = 0;
 var date = "todays date";
 var locationCurrent = "everywhere";
 var humidity = 0;
 var wind = 0;
 var UV = 0;
-var cityAry = ["Denver", "Des Moines", "Moscow"]
+var cityAry = ["Denver", "Des Moines", "Moscow"];
+var apiKey = "9bd07f7d4ce4fe8a1ea716aadf106115";
 //Function Creation
 
 //Function for when the search is used
@@ -14,7 +15,7 @@ function searchEnter() {
 
 //Function to populate current data to jumbotron
 function jumboPop() {
-    $("#tempCurrent").text("Temperature: " + temp);
+    $("#tempCurrent").text("Temperature: " + temperature + " F");
     $("#locationCurrent").text(locationCurrent + "(" + date + ")");
     $("#humidCurrent").text("Humidity: " + humidity);
     $("#windCurrent").text("Wind Speed: " + wind);
@@ -23,6 +24,7 @@ function jumboPop() {
 
 //Function to populate side bar of saved cities
 function sidePop() {
+    $("#sidebar").empty();
     cityAry.forEach(function(item){
         var button = $("<button>").attr({"type":"button", "class":"btn btn-secondary"});
         button.text(item);
@@ -31,15 +33,42 @@ function sidePop() {
     })
 }
 
-// //Function for when city on sidebar is clicked
-function sideClick() {
+// //Function for when city is entered into the input field
+function sideClick(event) {
+    event.preventDefault();
+    console.log(this);
+    cityAry.push($("#cityInput").val());
+    sidePop();
 
+}
+
+//Function for when the side buttons are clicked to populate
+function sideButtonClick(event) {
+    console.log(this);
+    locationCurrent = $(this).attr("data-city");
+    console.log(locationCurrent);
+    jumboPop();
+    pullCityInfo();
+}
+
+//function to make ajax calls for a city
+function pullCityInfo(){
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + locationCurrent + "&APPID=" + apiKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        console.log(response.main.temp);
+        temperature = ((response.main.temp)-273.15)*9/5 +32;
+        console.log(temperature);
+        jumboPop();
+    })
 }
 
 //Function Call outs
 jumboPop();
 sidePop();
 
-$("#sideBar-wrapper").on("click", "button", function(){
-    sidePop();
-})
+$("#sideBar-wrapper").on("click", "button", sideClick);
+$("#sidebar").on("click", "button", sideButtonClick);
