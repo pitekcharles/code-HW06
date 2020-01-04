e3beb2ef3f7d4b8c3779316a42ab99f9482a9b2b
unknown <charles.pitek@gmail.com>
2020-01-04 14:22:55 -0800

    removed unneeded console.logs and extra comments

--- script.js
+++ script.js
@@ -16,16 +16,7 @@ var temp5 = 0;
 var temp5day = [];
 var date5day = [];
 var humidity5day = [];
-
-
-//Function Creation
-
-//Function for when the search is used
-function searchEnter() {
-
-}
-
-//Function to populate current data to jumbotron  
+  
 function jumboPop() {
     $("#tempCurrent").text("Temperature: " + temperature + " F");
     $("#locationCurrent").text(locationCurrent + " (" + date + ")");
@@ -34,7 +25,6 @@ function jumboPop() {
     $("#UVCurrent").text("UV Index: " + UV);
 }
 
-//Function to populate side bar of saved cities
 function sidePop() {
     $("#sidebar").empty();
     cityAry.forEach(function (item) {
@@ -45,10 +35,8 @@ function sidePop() {
     })
 }
 
-// //Function for when city is entered into the input field
 function sideClick(event) {
     event.preventDefault();
-    // console.log(this);
     cityAry.push($("#cityInput").val());
     sidePop();
     locationCurrent = $("#cityInput").val();
@@ -56,51 +44,32 @@ function sideClick(event) {
 
 }
 
-//Function for when the side buttons are clicked to populate
 function sideButtonClick(event) {
-    // console.log(this);
     locationCurrent = $(this).attr("data-city");
-    // console.log(locationCurrent);
     pullCityInfo();
-    // console.log(UV)
-    // pullUVInfo();
-    // jumboPop();
-    // pop5Day();
 }
 
-//function to make ajax calls for a city
 function pullCityInfo() {
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + locationCurrent + "&APPID=" + apiKey;
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        // console.log(response);
         temperature = ((response.main.temp) - 273.15) * 9 / 5 + 32;
         temperature = temperature.toFixed(2);
         humidity = response.main.humidity;
         wind = response.wind.speed;
         latCurrent = response.coord.lat;
-        // console.log("lat " + latCurrent);
         lonCurrent = response.coord.lon;
         var queryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + latCurrent + "&lon=" + lonCurrent;
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            // console.log(response);
             UV = response.value;
             date = response.date_iso;
             var split = date.split("T");
-            // console.log(split);
             date = split[0];
-            // -----------------------------------------------------------------------
-            // $("#UVCurrent").text("UV Index: " + UV);
-            // $("#tempCurrent").text("Temperature: " + temperature + " F");
-            // $("#locationCurrent").text(locationCurrent + "(" + date + ")");
-            // $("#humidCurrent").text("Humidity: " + humidity + " %");
-            // $("#windCurrent").text("Wind Speed: " + wind + " MPH");
-            // -----------------------------------------------------------------------
             jumboPop();
             var queryURL3 = "http://api.openweathermap.org/data/2.5/forecast?appid=" + apiKey + "&q=" + locationCurrent;
             $.ajax({
@@ -114,16 +83,13 @@ function pullCityInfo() {
                     date5day[i] = dateTemp[0];
                     humidity5day[i] = response.list[4+(i*8)].main.humidity;
                 }
-               
                 pop5Day();
-                console.log("check");
             })
         })
     })
 
 }
 
-// Function populates info for 5 day forcast
 function pop5Day () {
     $(".wrapper5day").empty();
     for(var i=0; i < temp5day.length; i++){
